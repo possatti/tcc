a9fd0afaf7490c88ec41e2953ff37b3e4c993aaf
Lucas Possatti <lucas_possatti@hotmail.com>
2016-06-20 12:20:33 -0300

    Acerta a documentação de alguns scripts

--- scripts/read-outguess-capacity.sh
+++ scripts/read-outguess-capacity.sh
@@ -1,8 +1,8 @@
 #!/bin/sh
 
 ## Usage:
-##   outguess -k pass -d message.txt cover.jpg stego.jpg | sh this-script.sh`
-##  Outputs the capacity in bytes.
+##   `outguess -k pass -d message.txt cover.jpg stego.jpg 2>&1 | sh this-script.sh`
+##  Outputs the capacity in bytes. Remember to redirect stderr.
 
 bits=`sed -nr 's/Extracting usable bits:   ([[:digit:]]+) bits/\1/p'`
 expr $bits / 8  # convert to bytes
--- scripts/read-steghide-capacity.sh
+++ scripts/read-steghide-capacity.sh
@@ -1,4 +1,8 @@
 #!/bin/sh
 
+## Usage:
+##   `steghide info file.jpg | sh this-script.sh`
+##  Outputs the capacity in bytes.
+
 KB=`sed -nr 's/ +capacity: ([[:digit:]]+),0 KB/\1/p'`
 expr $KB '*' 1000  # convert to bytes
