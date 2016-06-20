e7d6ed4d602280e3808825f58f0a953e3d9eab3e
Lucas Possatti <lucas_possatti@hotmail.com>
2016-06-20 15:27:47 -0300

    Acerta o script que lê a capacidade esteganografica do outguess
    Eu estava lendo o número errado.

--- scripts/read-outguess-capacity.sh
+++ scripts/read-outguess-capacity.sh
@@ -4,5 +4,5 @@
 ##   `outguess -k pass -d message.txt cover.jpg stego.jpg 2>&1 | sh this-script.sh`
 ##  Outputs the capacity in bytes. Remember to redirect stderr.
 
-bits=`sed -nr 's/Extracting usable bits:   ([[:digit:]]+) bits/\1/p'`
+bits=`sed -nr 's/Correctable message size: ([[:digit:]]+) bits, .*%/\1/p'`
 expr $bits / 8  # convert to bytes
