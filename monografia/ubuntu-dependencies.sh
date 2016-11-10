6a9037275d2d33b85f55e8254b5fe6fded0b9275
Lucas Possatti <lucas_possatti@hotmail.com>
2016-11-10 02:32:25 -0200

    Retira a monografia de dentro do projeto
    Ela agora está no Overleaf.

--- monografia/ubuntu-dependencies.sh
+++
@@ -1,7 +0,0 @@
-#!bin/sh
-
-sudo apt-get -y install texlive-base
-sudo apt-get -y install texlive-publishers # Abntex2
-sudo apt-get -y install texlive-lang-french
-sudo apt-get -y install texlive-lang-portuguese
-sudo apt-get -y install texlive-lang-spanish
